354997a8756f1b4a143e656a3fa61ad45cd3a402
stanleyos <stanislaw.sarka@gmail.com>
2022-04-10 21:45:40 +0200

    added button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,23 @@
 import { useState } from 'react'
 import Card from './shared/Card'
+import Button from './shared/Button'
 
 const FeedbackForm = () => {
   const [text, setText] = useState('')
+  const [btnIsDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
+
   const handleTextInput = (e) => {
+    if (text === '') {
+      setMessage(null)
+      setBtnDisabled(true)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setBtnDisabled(false)
+      setMessage(null)
+    }
     setText(e.target.value)
   }
   return (
@@ -17,7 +31,11 @@ const FeedbackForm = () => {
             value={text}
             onChange={handleTextInput}
           />
+          <Button type="submit" isDisabled={btnIsDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{message} </div>}
       </form>
     </Card>
   )
